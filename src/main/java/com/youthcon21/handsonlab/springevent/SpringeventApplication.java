deac171adb38ffe0e5a2e666a63bec15b80dd6e6
tongnamuu <thban33@naver.com>
2021-12-19 02:13:29 +0900

    [step3] EnableAsync, Async

--- src/main/java/com/youthcon21/handsonlab/springevent/SpringeventApplication.java
+++ src/main/java/com/youthcon21/handsonlab/springevent/SpringeventApplication.java
@@ -2,8 +2,10 @@ package com.youthcon21.handsonlab.springevent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class SpringeventApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringeventApplication.class, args);
--- src/main/java/com/youthcon21/handsonlab/springevent/sender/application/SenderEventListener.java
+++ src/main/java/com/youthcon21/handsonlab/springevent/sender/application/SenderEventListener.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,11 +13,14 @@ public class SenderEventListener {
     private final static Logger log = LoggerFactory.getLogger(SenderEventListener.class);
 
     @EventListener
+    @Async
     public void handleEmail(UserSenderEvent event) {
-        log.info("환영 이메일 발송 성공 : {}", event.getEmail());
+        throw new RuntimeException();
+        // log.info("환영 이메일 발송 성공 : {}", event.getEmail());
     }
 
     @EventListener
+    @Async
     public void handleSMS(UserSenderEvent event) {
         log.info("환영 SMS 발송 성공 : {}", event.getPhoneNumber());
     }
