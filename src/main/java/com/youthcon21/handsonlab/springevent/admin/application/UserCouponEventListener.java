3c8c0f44cbc73fb881864a231e0c82fcf0d50a27
tongnamuu <thban33@naver.com>
2021-12-19 02:10:24 +0900

    [step3] TransactionalEventListener on CouponEventListener

--- src/main/java/com/youthcon21/handsonlab/springevent/admin/application/UserCouponEventListener.java
+++ src/main/java/com/youthcon21/handsonlab/springevent/admin/application/UserCouponEventListener.java
@@ -3,14 +3,14 @@ package com.youthcon21.handsonlab.springevent.admin.application;
 import com.youthcon21.handsonlab.springevent.user.event.UserCouponEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.ApplicationListener;
-import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 public class UserCouponEventListener {
     private final static Logger log = LoggerFactory.getLogger(UserCouponEventListener.class);
-    @EventListener
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void onApplicationEvent(UserCouponEvent event) {
         log.info("쿠폰 등록 완료 : {}", event.getEmail());
     }
